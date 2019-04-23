a2ed124f478eba5171a6d40f2356d22d1ad581b4
iBug <7273074+iBug@users.noreply.github.com>
2019-04-23 15:55:19 +0800

    use it again

--- src/main.c
+++ src/main.c
@@ -128,24 +128,8 @@ int main(int _argc, char * const * _argv) {
                             i += escape_char(&ch, cmd + i + 1);
                             parg[x++] = ch;
                         } else if (cmd[i] == '$') {
-                            char varname[MAX_VAR_NAME] = {};
-                            const char *varvalue;
-                            int j = 0; i += 1;
-                            for (;j < MAX_VAR_NAME; i++, j++) {
-                                if ((cmd[i] >= 'A' && cmd[i] <= 'Z') ||
-                                    (cmd[i] >= 'a' && cmd[i] <= 'z') ||
-                                    (cmd[i] >= '0' && cmd[i] <= '9') ||
-                                    cmd[i] == '_') {
-                                    varname[j] = cmd[i];
-                                } else break;
-                            }
-                            varvalue = get_variable(varname);
-                            if (varvalue) {
-                                strncpy(parg + x, varvalue, MAX_ARG_LEN - x - 1);
-                                parg[MAX_ARG_LEN - 1] = 0;
-                                x += strlen(parg + x);
-                            }
-                            i--;
+                            i += expand_token(parg + x, cmd + i, MAX_ARG_LEN - x - 1) - 1;
+                            for (; parg[x]; x++); // Move parg[x] to end of string
                             continue;
                         } else {
                             parg[x++] = cmd[i];
